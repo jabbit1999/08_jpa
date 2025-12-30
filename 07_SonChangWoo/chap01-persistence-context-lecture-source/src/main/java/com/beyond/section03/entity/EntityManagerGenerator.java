49b69e39fbede5bad8379093a486bd2dfc7e0f84
jabbit1999 <jabbit1999@gmail.com>
2025-12-30 17:28:58 +0900

    12/30 손창우 수업 내용 업로드

---
+++ 07_SonChangWoo/chap01-persistence-context-lecture-source/src/main/java/com/beyond/section03/entity/EntityManagerGenerator.java
@@ -0,0 +1,20 @@
+package com.beyond.section03.entity;
+
+import com.beyond.section01.entitymanager.EntityManagerFactoryGenerator;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class EntityManagerGenerator {
+
+  private static EntityManagerFactory factory
+      = Persistence.createEntityManagerFactory("jpatest");
+
+  public static EntityManager getInstance() {
+
+    // EntityManager 객체를 생성하여 반환
+    // -> 요청 마다 새로운 EntityManager 객체가 생성됨
+    return factory.createEntityManager();
+  }
+
+}
